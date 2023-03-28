146dca61abe95ba3bf7392e0da94a3cf8d0481fe
GiuseppeDaidone <aspis2000@virgilio.it>
2023-03-28 23:11:06 +0200

    Uploading sample app

---
+++ SampleApp/main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+    
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+    win.loadFile('index.html');
+    
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') {
+            app.quit();
+        }
+    });
+}
+    
+app.whenReady().then(() => {
+    createWindow();
+    app.on('activate', () => {
+      if(BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+      }
+    });
+});
