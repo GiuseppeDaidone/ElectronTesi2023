8101d97dd12fa9117d05606797e534e01ee19896
GiuseppeDaidone <aspis2000@virgilio.it>
2023-04-16 14:34:10 +0200

    Real-time communication with socket.io

--- Case study/ChatAppServer/main.js
+++ Case study/ChatAppServer/main.js
@@ -7,16 +7,42 @@ const iconPath = path.join(__dirname, 'icon.png');
 
 let tray = null;
 
+const { Server } = require('socket.io');
 const express = require('express');
 const http = require('http');
 const { electron } = require('process');
 
 const appServer = express();
 const server = http.Server(appServer);
-const port = process.env.PORT || 3000;
+const io = require('socket.io')(server, {
+  cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+      transports: ['websocket', 'polling'],
+      credentials: true
+  },
+  allowEIO3: true
+});
+const port = 3000;
 
 appServer.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket) {
+  socket.on("user_join", function(data) {
+      this.username = data;
+      socket.broadcast.emit("user_join", data);
+  });
+
+  socket.on("chat_message", function(data) {
+      data.username = this.username;
+      socket.broadcast.emit("chat_message", data);
+  });
+
+  socket.on("disconnect", function(data) {
+      socket.broadcast.emit("user_leave", this.username);
+  });
 });
 
 server.listen(port, function() {
